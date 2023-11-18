c60d326bd6db964c52ce6547a8a263a77f8df9b7
Jonathan Loske <03-tarpon.faeries@icloud.com>
2023-11-18 11:44:23 +0100

    feat: fix header for mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,16 @@
 export default function Index() {
   return (
-    <nav className="flex justify-between items-center center bg-white relative py-2.5 px-5">
-      <a href="/" className="db mr4 w-96 w4 flex-none">
+    <nav className="flex justify-between items-center bg-white py-2.5 px-5">
+      <a href="/" className="mr4 w-56 sm:w-96">
         <img src="/logo-banner.png" />
       </a>
 
-      <ul className="flex font-bold text-xl text-gray-500">
-        <li>
-          <a href="/contact" className="no-underline db">
-            Kontakt
-          </a>
-        </li>
-      </ul>
+      <a
+        href="/contact"
+        className="font-bold text-xl text-gray-500 no-underline hover:underline"
+      >
+        Kontakt
+      </a>
     </nav>
   );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -17,9 +17,7 @@ import rootStylesHref from "~/styles/root.css";
 
 export const meta: MetaFunction = () => [
   {
-    charset: "utf-8",
-    title: "New Remix App",
-    viewport: "width=device-width,initial-scale=1",
+    title: "Better Together DJ für Berlin und Brandenburg",
   },
 ];
 
@@ -32,6 +30,11 @@ export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
         <Meta />
         <Links />
         <link
