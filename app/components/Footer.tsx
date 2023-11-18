19b35aaf95138e350a12e0aff521c5653671c9ae
Jonathan Loske <03-tarpon.faeries@icloud.com>
2023-11-18 14:49:10 +0100

    adjust footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,34 +1,33 @@
+import InstagramIcon from "./icons/Instagram";
+
 export default function Index() {
   return (
-    <footer className="bg-black ph3 pv4 white">
-      <div className="mw7 center pt3">
-        <div className="flex-ns justify-between">
+    <footer className="bg-black px-5 py-2.5 text-white">
+      <div className="max-w-3xl text-center mx-auto">
+        <div className="sm:flex justify-between text-left">
           <div>
-            <h3 className="f4 b lh-title mb1 primary">
+            <h3 className="text-m mb-2.5 font-bold text-violet-600">
               Better Together DJ Berlin
             </h3>
-            <ul className="mb3">
+            <ul className="mb-3">
               <li>
-                <a href="/" className="link">
+                <a href="/" className="hover:underline">
                   Home
                 </a>
               </li>
               <li>
-                <a href="/about-us" className="link">
+                <a href="/about-us" className="hover:underline">
                   Über uns
                 </a>
               </li>
 
               <li>
-                <a href="/contact" className="link">
+                <a href="/contact" className="hover:underline">
                   Kontakt
-                </a>
-                <a href="/contact-helper" className="link">
-                  Kontakt Helper
                 </a>
               </li>
               <li>
-                <a href="/imprint" className="link">
+                <a href="/imprint" className="hover:underline">
                   Impressum
                 </a>
               </li>
@@ -36,16 +35,20 @@ export default function Index() {
           </div>
 
           <div>
-            <h3 className="f4 b lh-title mb2 primary">Social media</h3>
-            <ul className="mhn2">
-              <li className="dib ph2 raise">
+            <h3 className="text-m mb-2.5 font-bold text-violet-600">
+              Social media
+            </h3>
+            <ul className="">
+              <li className="inline-block">
                 <a
                   href="https://www.instagram.com/bettertogetherdj/"
-                  className="link bg-white black db relative br-100 pa2"
+                  className="link bg-white text-black block relative p-2 rounded-full"
                 >
-                  <svg width="16px" height="16px" className="db">
-                    <use xlinkHref="#icons-instagram"></use>
-                  </svg>
+                  <InstagramIcon
+                    width="1.5rem"
+                    height="1.5rem"
+                    className="block w-4 h-auto max-w-full max-h-full"
+                  />
                 </a>
               </li>
             </ul>
