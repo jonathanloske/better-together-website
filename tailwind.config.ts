4e3ef3580c8ecd3fed0e3eb426185d04354b5f89
Jonathan Loske <03-tarpon.faeries@icloud.com>
2023-11-18 17:16:55 +0100

    create /public/img structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/img/home-jumbotron.jpg')",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
