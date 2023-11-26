06e4972563f0ea552f32e4b1956ffa16d0f7b4c6
Jonathan Loske <03-tarpon.faeries@icloud.com>
2023-11-26 20:06:54 +0100

    Feinschliff

--- app/routes/about-us.tsx
+++ app/routes/about-us.tsx
@@ -9,7 +9,7 @@ export default function Index() {
             <h4 className="text-4xl font-bold">Über uns</h4>
           </div>
 
-          <div className="mb-4 flex flex-col gap-4 rounded-3xl bg-gradient-to-br from-blue-500 to-purple-950 p-6 lg:flex-row-reverse lg:bg-gradient-to-r lg:py-8 lg:pr-0">
+          <div className="align mb-4 flex flex-col items-center gap-4 rounded-3xl bg-gradient-to-br from-blue-500 to-purple-950 p-6 lg:flex-row-reverse lg:bg-gradient-to-r lg:py-8 lg:pr-0">
             {/* <!-- Bild: Portrait Vera & Jo--> */}
             <div className="relative lg:w-1/2">
               <img
@@ -22,12 +22,12 @@ export default function Index() {
             <div className="lg:w-1/2">
               {/* <!-- Text über uns --> */}
 
-              <h2 className="text-xl font-bold">Hi!</h2>
+              <h2 className="mb-2 text-xl font-bold">Hi!</h2>
 
               <p className="mb-2">
-                Wir sind Jonathan & Vera und zusammen sind wir Better Together
-                💜 Wir leben in Berlin und sind für euch in der Region
-                Berlin-Brandenburg aktiv.
+                Wir sind Jonathan & Vera und zusammen sind wir
+                Better&nbsp;Together&nbsp;💜 Wir leben in Berlin und sind für
+                euch in der Region Berlin-Brandenburg aktiv.
               </p>
 
               <p>
@@ -44,7 +44,7 @@ export default function Index() {
             </div>
           </div>
 
-          <div className="mb-4 px-6">
+          <div className="mb-4 max-w-lg px-6 lg:mx-auto">
             <p className="mb-2">
               Da dachten wir: Hey, das muss doch auch besser gehen - und am
               besten zusammen! #bettertogether Im Gegensatz zu den meisten
@@ -70,9 +70,9 @@ export default function Index() {
           </div>
 
           <div className="">
-            <div className="mb-4 flex flex-col gap-4 rounded-3xl bg-gradient-to-tr from-blue-500 to-purple-950 p-6 lg:ml-56 lg:flex-row-reverse lg:bg-gradient-to-l lg:py-8 lg:pr-0">
+            <div className="mb-4 flex flex-col items-center gap-4 rounded-3xl bg-gradient-to-tr from-blue-500 to-purple-950 p-6 lg:ml-auto lg:max-w-3xl lg:flex-row-reverse lg:justify-between lg:bg-gradient-to-l lg:py-8 lg:pr-0">
               {/* <!-- Bild: Jonathan --> */}
-              <div className="relative lg:w-1/2">
+              <div className="relative lg:w-1/2 lg:max-w-sm">
                 <img
                   src="img/better-together-dj-berlin-portrait-jo.jpg"
                   alt="Better Together - euer DJ in Berlin & Brandenburg. Portrait von Jonathan. Er steht hinter seinem DJ-Pult, das mit Pflanzen und einer Diskokugel geschmückt ist. Er trägt ein farbenfrohes T-Shirt."
@@ -93,8 +93,8 @@ export default function Index() {
               </p>
             </div>
 
-            <div className="mb-4 flex flex-col gap-4 rounded-3xl bg-gradient-to-bl from-blue-500 to-purple-950 p-6 lg:mr-56 lg:flex-row lg:bg-gradient-to-r lg:py-8 lg:pl-0">
-              <div className="relative lg:w-1/2">
+            <div className="mb-4 flex flex-col items-center gap-4 rounded-3xl bg-gradient-to-bl from-blue-500 to-purple-950 p-6 lg:max-w-3xl lg:flex-row lg:justify-between lg:bg-gradient-to-r lg:py-8 lg:pl-0">
+              <div className="relative lg:max-w-sm">
                 {/* <!-- Bild: Vera--> */}
                 <img
                   src="img/better-together-dj-berlin-portrait-vera-fullbody-square.jpg"
