3bc92458b7e3620382723fa00c6212543d654b6b
Jonathan Loske <03-tarpon.faeries@icloud.com>
2023-11-30 23:38:10 +0100

    update meta-favicon
    and whatsapp description

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,7 @@ import rootStylesHref from "~/styles/root.css";
 
 export const meta: MetaFunction = () => [
   {
+    // max 65 characters
     title: "Better Together DJ für Berlin und Brandenburg",
   },
 ];
@@ -31,10 +32,54 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        {/* For more help on these meta tags, see https://stackoverflow.com/a/43154489 */}
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
+        {/* max 155 characters */}
+        <meta
+          name="description"
+          content="Better Together ist euer DJ-Service für Berlin und Brandenburg von Jonathan Loske und Vera Loske"
+        />
+        {/* max 35 characters */}
+        <meta property="og:title" content="Better Together DJ (Berlin + BB)" />
+        <meta property="og:url" content="https://better-together-dj.com/" />
+        {/* max 65 characters */}
+        <meta
+          property="og:description"
+          content="Better Together ist euer DJ-Service für Berlin und Brandenburg"
+        />
+        <meta property="og:image" content="/social-image.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="de_DE" />
+        {/* Favicons generated with the help of https://realfavicongenerator.net */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png?v=1"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png?v=1"
+        />
+        <link rel="manifest" href="/site.webmanifest?v=1" />
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg?v=1"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <Meta />
         <Links />
         <link
